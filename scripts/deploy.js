8cc89000ee71d7dc709921505c45803d76cb9a03
freddyal0x01 <freddyal0x01@gmail.com>
2023-01-31 21:22:47 -0500

    Finished Project 4 emit winner script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,16 +9,16 @@ const hre = require("hardhat");
 const CONTRACT_ADDR = "0xcF469d3BEB3Fc24cEe979eFf83BE33ed50988502";
 
 async function main() {
-  const Win = await hre.ethers.getContractAt("Win", CONTRACT_ADDR);
+  const Win = await hre.ethers.getContractFactory("Win");
   
-  const win = await Win.win();
+  const win = await Win.deploy(CONTRACT_ADDR);
 
-  await win.deployed();
+  await win.win()
 
   console.log(
-    `Contract deployed from ${win.address} deployed to ${CONTRACT_ADDR}`
+    `Contract deployed from ${win.address} & emitting an event over to ${CONTRACT_ADDR}`
   );
-}
+};
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
